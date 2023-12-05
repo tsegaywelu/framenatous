62e69681062b11d6e9dea006251183333e26655d
Tsegay Welu <tsegaygebrezgi56@gmail.com>
2023-12-04 21:27:54 -0800

    login registration adminhome and home works perfectly

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -2,9 +2,6 @@
  const child=require('./model/childmodel');
 /*  const express=require('express')
  const app=express() */
- const path = require('path');
-app.use(express.static(path.join(__filename, 'home.html', 'public')));
-
  
 
 class usercontroller {
